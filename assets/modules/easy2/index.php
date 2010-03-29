c3606d4d488b999f6ecbbb2b14e313f121cf6994
curiositymind <curiositymind@b755a610-95fd-11de-9a8c-5f207592b8ba>
2010-03-29 04:28:07 +0000

    some code commented clean ups

--- assets/modules/easy2/index.php
+++ assets/modules/easy2/index.php
@@ -7,11 +7,10 @@ if (IN_MANAGER_MODE != 'true') die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Ple
  * @author Cx2 <inteldesign@mail.ru>
  * @author Temus
  * @author goldsky <goldsky@modx-id.com>
- * @version 1.3.6
  */
 
 // Easy 2 Gallery version
-define('E2G_VERSION', '1.3.6 - Beta 3');
+define('E2G_VERSION', '1.4.0 - Beta 1');
 
 // Easy 2 Gallery module path
 if(!defined('E2G_MODULE_PATH')) {
@@ -42,9 +41,9 @@ if (file_exists( E2G_MODULE_PATH . 'langs/'.$modx->config['manager_language'].'.
 mysql_select_db(str_replace('`', '', $GLOBALS['dbase']));
 @mysql_query("{$GLOBALS['database_connection_method']} {$GLOBALS['database_connection_charset']}");
 
-//// ALERTS / ERRORS
-//if (!isset( $_SESSION['easy2err'] ) ) $_SESSION['easy2err'] = array();
-//if (!isset( $_SESSION['easy2suc'] ) ) $_SESSION['easy2suc'] = array();
+// ALERTS / ERRORS
+if (!isset( $_SESSION['easy2err'] ) ) $_SESSION['easy2err'] = array();
+if (!isset( $_SESSION['easy2suc'] ) ) $_SESSION['easy2suc'] = array();
 
 require E2G_MODULE_PATH . 'config.easy2gallery.php';
 // Install
@@ -52,35 +51,21 @@ if (is_dir( E2G_MODULE_PATH . 'install')) {
     require_once E2G_MODULE_PATH . 'install/index.php';
     exit();
 }
-//$e2g['mdate_format'] = 'd-m-y H:i';
-//if (!is_dir( MODX_BASE_PATH . $e2g['dir'])) {
-//    echo '<b style="color:red">'.$lng['dir'].' &quot;'.$e2g['dir'].'&quot; '.$lng['empty'].'</b>';
-//    exit;
-//} elseif (!is_dir( MODX_BASE_PATH . $e2g['dir'] . '_thumbnails' ) ) {
-//    if (mkdir( MODX_BASE_PATH . $e2g['dir'] . '_thumbnails' ) ) {
-//        @chmod( MODX_BASE_PATH . $e2g['dir'] . '_thumbnails', 0755 );
-//    } else {
-//        echo '<b style="color:red">' . $lng['_thumb_err'] . '</b>';
-//        exit;
-//    }
-//}
-//
-//$gdir = $e2g['dir'];
-//$path = '';
-//$parent_id = ( isset( $_GET['pid'] ) && is_numeric( $_GET['pid'] ) ) ? (int) $_GET['pid'] : 1;
+
+$gdir = $e2g['dir'];
+$path = '';
+$parent_id = ( isset( $_GET['pid'] ) && is_numeric( $_GET['pid'] ) ) ? (int) $_GET['pid'] : 1;
 
 /*
  * EXECUTE MODULE
 */
 
-//include_once 'includes/pane.main.inc.php';
 if(!class_exists('e2g_mod')) {
     include_once E2G_MODULE_PATH . "classes/e2g.module.class.php";
 }
 if (class_exists('e2g_mod')) {
     $e2g_mod = new e2g_mod($e2gmod_cl, $e2g, $lng);
     $output = $e2g_mod;
-//    $output = $e2g_mod->explore();
 } else {
     $output = "<h3>error: e2g_snip class not found</h3>";
 }
