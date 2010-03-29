422bf891e4441980862f144f4c65b0472a694514
curiositymind <curiositymind@b755a610-95fd-11de-9a8c-5f207592b8ba>
2010-03-29 04:34:47 +0000

    clean ups

--- assets/modules/easy2/includes/pane.files.inc.php
+++ assets/modules/easy2/includes/pane.files.inc.php
@@ -6,12 +6,6 @@ if (IN_MANAGER_MODE != 'true') die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Ple
     <script type="text/javascript">
         tpResources.addTabPage(document.getElementById('imManager'));
     </script>
-    <form action="<?php echo $index; ?>&act=synchro" method="post">
-        <input type="submit" value="<?php echo $lng['synchro']; ?>" />
-    </form>
     <br />
     <?php echo $content; ?>
-    <form action="<?php echo $index; ?>&act=synchro" method="post">
-        <input type="submit" value="<?php echo $lng['synchro']; ?>" />
-    </form>
 </div>
