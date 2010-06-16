e3fd7db6464c2d78e67fbefd96c8b14eb65ceaa4
curiositymind <curiositymind@b755a610-95fd-11de-9a8c-5f207592b8ba>
2010-06-16 11:49:14 +0000

    updated the russian language files

--- assets/modules/easy2/includes/langs/russian-UTF8.comments.php
+++ assets/modules/easy2/includes/langs/russian-UTF8.comments.php
@@ -1,17 +1,20 @@
 <?php
+/** Translated by Temus (temus3@gmail.com) **/
 
 $e2g_lang['russian-UTF8'] = array (
-'title' => 'Комментарии',
-'comment_add' => 'Добавить комментарий',
-'name' => 'Имя',
-'email' => 'Email',
-'usercomment' => 'Комментарий',
-'send_btn' => 'Отправить',
-'code' => 'Код',
-'empty_name_comment' => 'Не указано имя или комментарий',
-'comment_added' => 'Комментарий добавлен',
-'comment_add_err' => 'Комментарий не добавлен',
-'pages' => 'Страницы',
-'captcha_err' => 'Неверный защитный код',
+        'captcha_err' => 'Неверный защитный код',
+        'code' => 'Код',
+        'comment_add' => 'Добавить комментарий',
+        'comment_add_err' => 'Комментарий не добавлен',
+        'comment_added' => 'Комментарий добавлен',
+        'email' => 'Email',
+        'email_err' => 'Неверный email',
+        'empty_name_comment' => 'Не указано имя или комментарий',
+        'name' => 'Имя',
+        'pages' => 'Страницы',
+        'send_btn' => 'Отправить',
+        'title' => 'Комментарии',
+        'usercomment' => 'Комментарий',
 );
 ?>
+
