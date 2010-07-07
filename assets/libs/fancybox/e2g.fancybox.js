31d4e6e8bbc9eea820d680d3c9c246b9c9d2b13c
curiositymind <curiositymind@b755a610-95fd-11de-9a8c-5f207592b8ba>
2010-07-07 07:58:05 +0000

    updated javascript libraries (fancybox)

--- assets/libs/fancybox/e2g.fancybox.js
+++ assets/libs/fancybox/e2g.fancybox.js
@@ -1,6 +1,8 @@
 // AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
 jQuery(function($) {
-	$("a.iframe").fancybox({
-		'hideOnContentClick': false
-	});
+    $("a.comment").fancybox({
+        'transitionIn'  : 'none',
+        'transitionOut' : 'none',
+        'type'          : 'iframe'
+    });
 });
