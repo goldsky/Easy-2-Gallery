180dbbf4272767f27f80676cb98e74da6192a93c
curiositymind <curiositymind@b755a610-95fd-11de-9a8c-5f207592b8ba>
2010-07-12 02:27:03 +0000

    changed the group rel title

--- assets/libs/colorbox/e2g.colorbox.js
+++ assets/libs/colorbox/e2g.colorbox.js
@@ -1,5 +1,5 @@
 // AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
 jQuery(function($) {
-	$("a[rel^='lightbox']").colorbox({photo:true});
+	$("a[rel^='group']").colorbox({photo:true});
 	$(".iframe").colorbox({width:450, height:380, iframe:true,opacity:0.3});
 });
