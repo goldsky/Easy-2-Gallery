968105fcf3bbceed57b34d2929b6c16ab162edcd
curiositymind <curiositymind@b755a610-95fd-11de-9a8c-5f207592b8ba>
2010-08-24 00:30:47 +0000

    disable charset header in module bugfix module css

--- assets/modules/easy2/includes/tpl/pane.main.inc.php
+++ assets/modules/easy2/includes/tpl/pane.main.inc.php
@@ -1,11 +1,16 @@
 <?php
-header('content-type: text/html;'.$lng['charset']);
-echo $lng['setlocale_locale'];
+// disable for a reason:
+// http://modxcms.com/forums/index.php/topic,23177.msg308887.html#msg308887
+//header('content-type: text/html;' . $lng['charset']);
+$setlocale = @explode(',', trim(trim($lng['setlocale'], "setlocale("), ')'));
+call_user_func_array('setlocale', array(constant(trim($setlocale[0])), trim($setlocale[1])));
 
-if (IN_MANAGER_MODE != 'true') die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODx Content Manager instead of accessing this file directly.");
+if (IN_MANAGER_MODE != 'true')
+    die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODx Content Manager instead of accessing this file directly.");
 $_t = $this->e2gmod_cfg['_t'];
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <title>Easy 2 Gallery <?php echo E2G_VERSION; ?></title>
@@ -30,9 +35,9 @@ $_t = $this->e2gmod_cfg['_t'];
                 var im = document.getElementById("imFields");
                 var di = document.createElement("DIV");
                 var fi = document.getElementById("firstElt");
-                di.innerHTML = '<a href="#" onclick="this.parentNode.parentNode.removeChild(this.parentNode);" style="color:red;text-decoration:none;"><b style="letter-spacing:4px"> &times; <?php echo $lng['remove']; ?></b></a>'+fi.innerHTML;
+                di.innerHTML = '<a href="javascript:;" onclick="this.parentNode.parentNode.removeChild(this.parentNode);" style="color:red;text-decoration:none;"><b style="letter-spacing:4px"> &times; <?php echo $lng['remove']; ?><\/b><\/a>'+fi.innerHTML;
                 im.appendChild(di);
-                return TRUE;
+                return true;
             }
             // for download preview
             function uimPreview (imSrc) {
@@ -55,9 +60,9 @@ $_t = $this->e2gmod_cfg['_t'];
                 if ( w == 0 || h == 0 ) {
                     imBox.innerHTML = '<?php echo $lng['uim_preview_err']; ?>';
                 } else {
-                    imBox.innerHTML = '<img src="'+imSrc+'" width="'+w+'" height="'+h+'" />';
+                    imBox.innerHTML = '<img src="'+imSrc+'" width="'+w+'" height="'+h+'" alt="" />';
                 }
-                return TRUE;
+                return true;
             }
             function selectAll (check_var) {
                 for (var i=0; i<document.forms["list"].elements.length; i++) {
@@ -69,43 +74,43 @@ $_t = $this->e2gmod_cfg['_t'];
                 if (i==1) {
                     var index=document.forms["topmenu"].newparent.selectedIndex;
                     if (document.forms["topmenu"].newparent.options[index].value != "") {
-                        window.location.href= '<?php echo $index; ?>&pid='+ document.forms["topmenu"].newparent.options[index].value;
+                        window.location.href= '<?php echo html_entity_decode($index); ?>&pid='+ document.forms["topmenu"].newparent.options[index].value;
                     }
                 }
                 if (i=='1b') {
                     document.forms["topmenu"].action=
-                    "<?php echo $index; ?>&pid=<?php echo $_POST['newparent']; ?>&page=openexplorer";
+                        "<?php echo html_entity_decode($index); ?>&pid=<?php echo $_POST['newparent']; ?>&page=openexplorer";
                     document.forms["topmenu"].submit();
                 }
                 if (i==2) {
                     var index=document.forms["topmenu"].opentag.selectedIndex;
                     if (document.forms["topmenu"].opentag.options[index].value != "") {
-                        window.location.href= '<?php echo $index; ?>&page=tag&tag='+ document.forms["topmenu"].opentag.options[index].value;
+                        window.location.href= '<?php echo html_entity_decode($index); ?>&page=tag&tag='+ document.forms["topmenu"].opentag.options[index].value;
                     }
                 }
                 if (i==3) {
                     document.forms["list"].action=
-                    "<?php echo $index.'&act=delete_checked&pid='.$parent_id.(!empty($cpath)?'&path='.$cpath:'');?>";
+                        "<?php echo html_entity_decode($index) . '&act=delete_checked&pid=' . $parent_id . (!empty($cpath) ? '&path=' . $cpath : ''); ?>";
                     document.forms["list"].submit();
                 }
                 if (i==4) {
                     document.forms["list"].action=
-                    "<?php echo $index.'&act=download_checked&pid='.$parent_id.(!empty($cpath)?'&path='.$cpath:'');?>";
+                        "<?php echo html_entity_decode($index) . '&act=download_checked&pid=' . $parent_id . (!empty($cpath) ? '&path=' . $cpath : ''); ?>";
                     document.forms["list"].submit();
                 }
                 if (i==5) {
                     document.forms["list"].action=
-                    "<?php echo $index.'&act=move_checked&pid='.$parent_id.(!empty($cpath)?'&path='.$cpath:'');?>";
+                        "<?php echo html_entity_decode($index) . '&act=move_checked&pid=' . $parent_id . (!empty($cpath) ? '&path=' . $cpath : ''); ?>";
                     document.forms["list"].submit();
                 }
                 if (i==6) {
                     document.forms["list"].action=
-                    "<?php echo $index.'&act=tag_add_checked&pid='.$parent_id.(!empty($cpath)?'&path='.$cpath:'');?>";
+                        "<?php echo html_entity_decode($index) . '&act=tag_add_checked&pid=' . $parent_id . (!empty($cpath) ? '&path=' . $cpath : ''); ?>";
                     document.forms["list"].submit();
                 }
                 if (i==7) {
                     document.forms["list"].action=
-                    "<?php echo $index.'&act=tag_remove_checked&pid='.$parent_id.(!empty($cpath)?'&path='.$cpath:'');?>";
+                        "<?php echo html_entity_decode($index) . '&act=tag_remove_checked&pid=' . $parent_id . (!empty($cpath) ? '&path=' . $cpath : ''); ?>";
                     document.forms["list"].submit();
                 }
             }
@@ -223,44 +228,44 @@ $_t = $this->e2gmod_cfg['_t'];
                 }
             }
         </script>
-        <script type="text/javascript" src="<?php echo MODX_BASE_URL.$e2g['tinymcefolder'];?>/tiny_mce.js"></script>
+        <script type="text/javascript" src="<?php echo MODX_BASE_URL . $e2g['tinymcefolder']; ?>/tiny_mce.js"></script>
         <script type="text/javascript">
-        tinyMCE.init({
-            mode : "textareas",
-            theme : "advanced",
-            editor_selector : "mceEditor",
-            theme_advanced_toolbar_location : "top",
-            theme_advanced_toolbar_align : "left",
-            theme_advanced_statusbar_location : "bottom",
-            theme_advanced_resizing : true,
-            content_css : "<?php echo E2G_MODULE_URL; ?>includes/tpl/e2g_mod.css"
-        });
+            tinyMCE.init({
+                mode : "textareas",
+                theme : "advanced",
+                editor_selector : "mceEditor",
+                theme_advanced_toolbar_location : "top",
+                theme_advanced_toolbar_align : "left",
+                theme_advanced_statusbar_location : "bottom",
+                theme_advanced_resizing : true,
+                content_css : "<?php echo E2G_MODULE_URL; ?>includes/tpl/e2g_mod.css"
+            });
         </script>
     </head>
     <body>
-<?php
+        <?php
         $suc = $err = '';
         if (count($_SESSION['easy2err']) > 0) {
-            $err = '<p class="warning" style="padding-left: 10px;">'.implode('<br />', $_SESSION['easy2err']).'</p>';
+            $err = '<p class="warning" style="padding-left: 10px;">' . implode('<br />', $_SESSION['easy2err']) . '</p>';
             $_SESSION['easy2err'] = array();
         }
         if (count($_SESSION['easy2suc']) > 0) {
-            $suc = '<p class="success" style="padding-left: 10px;">'.implode('<br />', $_SESSION['easy2suc']).'</p>';
+            $suc = '<p class="success" style="padding-left: 10px;">' . implode('<br />', $_SESSION['easy2suc']) . '</p>';
             $_SESSION['easy2suc'] = array();
         }
-?>
-        <p><?php echo $err.$suc; ?></p>
+        ?>
+        <p><?php echo $err . $suc; ?></p>
         <div class="sectionHeader">Easy 2 Gallery <?php echo E2G_VERSION; ?></div>
         <div class="sectionBody">
             <div class="tab-pane" id="easy2Pane">
                 <script type="text/javascript">
                     tpResources = new WebFXTabPane(document.getElementById('easy2Pane'));
                 </script>
-                <?php include_once 'pane.files.inc.php';?>
-                <?php include_once 'pane.imageupload.inc.php';?>
-                <?php include_once 'pane.comments.inc.php';?>
-                <?php include_once 'pane.config.inc.php';?>
-                <?php include_once 'pane.help.inc.php';?>
+                <?php include_once 'pane.files.inc.php'; ?>
+                <?php include_once 'pane.imageupload.inc.php'; ?>
+                <?php include_once 'pane.comments.inc.php'; ?>
+                <?php include_once 'pane.config.inc.php'; ?>
+<?php include_once 'pane.help.inc.php'; ?>
             </div>
         </div>
     </body>
